bdacc536129683796f7893c8fec7367b008d6fee
RAJESH RANJAN <64878501+Rajesh42@users.noreply.github.com>
2022-06-25 23:34:15 +0530

    Create Day2.cpp

---
+++ Day2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //M1 
+        int ans = nums[0];
+         for (int i=1;i<nums.size();i++){
+              ans = ans^nums[i];
+         }
+         return ans;
+      
+        //M2
+        /*unordered_map<int,int>um;
+        for (int i=0;i<nums.size();i++){
+             um[nums[i]]++;
+        }
+        for(auto x:um){
+            if (x.second %2!= 0) return x.first;
+        }
+        return nums[0];*/
+    }
+};
