e033b5af51d1be6d2c0b120fc84348e12d578ca3
RAJESH RANJAN <64878501+Rajesh42@users.noreply.github.com>
2022-06-26 20:27:59 +0530

    Update Day2.cpp

--- Day2.cpp
+++ Day2.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         //M1 
-        int ans = nums[0];
+        /*int ans = nums[0];
          for (int i=1;i<nums.size();i++){
               ans = ans^nums[i];
          }
-         return ans;
-      
-        //M2
-        /*unordered_map<int,int>um;
+         return ans;*/
+        
+        /*M2
+        unordered_map<int,int>um;
         for (int i=0;i<nums.size();i++){
              um[nums[i]]++;
         }
@@ -17,5 +17,10 @@ public:
             if (x.second %2!= 0) return x.first;
         }
         return nums[0];*/
+        
+        //M3
+        int ans =0;
+        for(int x:nums)ans = x^ans;
+        return ans;
     }
 };
