e1fcab9bbd6db94a1df02abcbd85971c6a8f6983
RAJESH RANJAN <64878501+Rajesh42@users.noreply.github.com>
2022-07-05 21:49:20 +0530

    Create Day12.cpp

---
+++ Day12.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int a =0,b=0,c=0;
+        for(int x:nums){
+            if(x==0)a++;
+            else if(x == 1)b++;
+            else c++;
+        }
+        int i=0;
+        while(a){nums[i]=0;a--;i++;}
+        while(b){nums[i]=1;b--;i++;}
+        while(c){nums[i]=2;c--;i++;}
+    }
+};
