0c17adfd3ff7e9a67abebede9e66cd7ca99bbc38
RAJESH RANJAN <64878501+Rajesh42@users.noreply.github.com>
2022-07-10 20:37:31 +0530

    Update Day17.cpp

--- Day17.cpp
+++ Day17.cpp
@@ -1,3 +1,20 @@
+//M-01
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head)return false;
+        unordered_set<ListNode*>us;
+        while(head){
+            if(us.find(head)!=us.end()){
+                return true;
+            }
+            us.insert(head);
+            head = head->next;
+        }
+        return false;
+    }
+};
+//M-02
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
