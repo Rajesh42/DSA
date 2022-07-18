a601997aacefcfed2d3dfa6e46e5edae0bcdd9e8
RAJESH RANJAN <64878501+Rajesh42@users.noreply.github.com>
2022-07-18 11:25:34 +0530

    Update Day23.cpp

--- Day23.cpp
+++ Day23.cpp
@@ -14,25 +14,29 @@ public:
 
 #M-2
 ----------------------------------------------
-Node* firstnode(Node* head)
-{
-    if (head == NULL || head->next == NULL)
-        return NULL;
-    Node *slow = head, *fast = head;
-    slow = slow->next;
-    fast = fast->next->next;
-    while (fast && fast->next) {
-        if (slow == fast)
-            break;
-        slow = slow->next;
-        fast = fast->next->next;
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)break;
+        }
+        
+        if(fast==NULL || fast->next==NULL)return NULL;
+        
+        ListNode* ptr1=head;
+        ListNode* ptr2=slow;
+        
+        while(ptr1!=ptr2){
+            ptr1=ptr1->next;
+            ptr2=ptr2->next;
+        }
+        
+        return ptr1;   
     }
-    if (slow != fast)
-        return NULL;
-    slow = head;
-    while (slow != fast) {
-        slow = slow->next;
-        fast = fast->next;
-    }
-    return slow;
-}
+};
