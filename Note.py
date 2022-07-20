89972f072acf7cc6334fe7d4bf1c154602f9d3a0
RAJESH RANJAN <64878501+Rajesh42@users.noreply.github.com>
2022-07-20 12:42:48 +0530

    Create Note.py

---
+++ Note.py
@@ -0,0 +1,3 @@
+
+1. AI Developer (Working experience with ONNX Runtime, OpenVINO, OpenVINO-Tensorflow repositories, and automating various deep learning model validations using Jenkins and shell scripting for multiple devices, execution platforms, and Linux flavors)
+2. Mendix Developer (created Microflows and Nanoflows for Mobile Apps and Websites, in addition to page and entity level security) 
